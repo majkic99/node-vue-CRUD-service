45e9879c6d5696c4a8beef836d88475b11e6ae5b
majkic99 <60391069+majkic99@users.noreply.github.com>
2020-11-14 21:21:40 +0100

    vue installed

--- fantazifudbal/node/app.js
+++ fantazifudbal/node/app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const dbase = require('./dbapi/dbapi');
+const history = require('connect-history-api-fallback');
+const path = require('path');
+
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET", "PUT", "POST", "DELETE", "OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/api', dbase);
+
+const staticDir = express.static(path.join(__dirname, 'dist'));
+
+app.use(staticDir);
+app.use(history);
+app.use(staticDir);
 
 app.listen(81);
